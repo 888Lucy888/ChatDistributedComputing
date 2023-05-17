6070abffa1a22a3564eb587438a04ddd6795d0fa
888Lucy888 <chuggaaconroy888@gmail.com>
2023-05-16 19:14:22 -0600

    Encryption w/numbers

--- Talkie/src/UI/LoginController.java
+++ Talkie/src/UI/LoginController.java
@@ -156,21 +156,62 @@ public class LoginController implements Initializable {
         }
     }
 
-    public static String caesarEncrypt(int shift, String plaintext) {
-        StringBuilder ciphertext = new StringBuilder();
+public static String caesarEncrypt(int shift, String plaintext) {
+        StringBuilder encryptedText = new StringBuilder();
+        
         for (int i = 0; i < plaintext.length(); i++) {
             char c = plaintext.charAt(i);
-            if (Character.isLetter(c)) {
-                int base = Character.isLowerCase(c) ? 'a' : 'A';
+            
+            if (Character.isLetter(c) || Character.isDigit(c)) {
+                int base;
+                if (Character.isLowerCase(c)) {
+                    base = 'a';
+                } else if (Character.isUpperCase(c)) {
+                    base = 'A';
+                } else {
+                    if((int)c>52){
+                        encryptedText.append((char)(c-5));
+                    }else{
+                        encryptedText.append((char)(c+5));
+                    }
+                    continue;
+                }
                 c = (char)(((int)c - base + shift) % 26 + base);
             }
-            ciphertext.append(c);
+            
+            encryptedText.append(c);
         }
-        return ciphertext.toString();
+        
+        return encryptedText.toString();
     }
 
-    public static String caesarDecrypt(int shift, String ciphertext) {
-        return caesarEncrypt(26 - shift,ciphertext);
+public static String caesarDecrypt(int shift, String ciphertext) {
+        StringBuilder decryptedText = new StringBuilder();
+        
+        for (int i = 0; i < ciphertext.length(); i++) {
+            char c = ciphertext.charAt(i);
+            
+            if (Character.isLetter(c) || Character.isDigit(c)) {
+                int base;
+                if (Character.isLowerCase(c)) {
+                    base = 'a';
+                } else if (Character.isUpperCase(c)) {
+                    base = 'A';
+                } else {
+                    if((int)c>52){
+                        decryptedText.append((char)(c-5));
+                    }else{
+                        decryptedText.append((char)(c+5));
+                    }
+                    continue;
+                }
+                c = (char)(((int)c - base - shift + 26) % 26 + base);
+            }
+            
+            decryptedText.append(c);
+        }
+        
+        return decryptedText.toString();
     }
     
 }
